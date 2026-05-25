48c5173a44b5eb22da2858401fe2792c8ffa5f18
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2026-05-25 21:24:41 +0200

    Local works, online doesnt

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
+export { Fractal } from "./fractals/fractal.js";
 export { Mandelbrot } from "./fractals/mandelbrot.js";
 export { Julia } from "./fractals/julia.js";
 export { Multibrot } from "./fractals/multibrot.js";
 export { Nova } from "./fractals/nova.js";
-export { Fractal } from "./fractals/fractal.js";
 //# sourceMappingURL=index.d.ts.map
